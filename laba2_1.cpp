16d6c78d8d7d965c3954e17995efd2a8b5a99c79
Admin <Admin@DESKTOP-HBER6S9>
2022-10-19 22:23:52 +0300

    Добавьте файлы проекта.

---
+++ laba2_1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	double a, b;
+	setlocale(LC_ALL, "");
+	cout << "Введите a= ";
+	cin >> a;
+	cout << "Введите b= ";
+	cin >> b;
+	cout << "a-b=" << a - b << endl;
+	cout << "a/b=" << a / b << endl;
+	return 0;
+
+}
